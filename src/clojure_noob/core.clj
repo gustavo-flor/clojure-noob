42b9917c3536957f48746a759b57ebe31a070ee8
gustavo-flor <gustavo.flor@picpay.com>
2022-01-25 14:37:28 -0300

    :wrench: Using some operators and understanding how they work

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -3,7 +3,32 @@
 
 (def message "I'm a little teapot!")
 
+(def character "Chewbacca")
+
+(def villains ["Lord Voldemort" "Thanos" "Darth Vader"])
+
+(def person {:name "Gustavo Flôr"
+             :age 20
+             :role "Software Engineer"})
+
+(defn create-person
+  [name, age, role]
+  {:name name
+   :age age
+   :role role})
+
+(def other-person (create-person "Pedro Scooby", 30, "Surfer"))
+
 (defn -main
   "I don't do a lot ... yet."
   [& args]
-  (println message))
+  (println message)
+  (println (str "\"Uggllglglglglglglll\" - " character))
+  (println (first villains))
+  (println (last villains))
+  (println (get villains 1))
+  (println (get villains (- (count villains) 2)))
+  (println (if args 1 0))
+  (println (get person :name))
+  (println other-person)
+  (println (* 1 2 3 4)))
