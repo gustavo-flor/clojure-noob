0e22c0960e6f10072e45786b4fd6a73ce1b95fd0
gustavo-flor <gustavo.flor@picpay.com>
2022-01-27 08:50:50 -0300

    :wrench: Using Clojure Arity

--- src/clojure_noob/fibonacci.clj
+++ src/clojure_noob/fibonacci.clj
@@ -2,7 +2,8 @@
 
 (defn calc
   "Calculate Fibonacci value by potency"
-  [potency]
+  ([] 0)
+  ([potency]
   (if (<= potency 1)
     potency
-    (+ (calc (- potency 1)) (calc (- potency 2)))))
+    (+ (calc (- potency 1)) (calc (- potency 2))))))
