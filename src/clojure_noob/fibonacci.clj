4425659c4e8c89b074e4e5b8b12bff29e8e0030e
gustavo-flor <gustavo.flor@picpay.com>
2022-01-27 08:51:33 -0300

    :wrench: Refactoring file indent

--- src/clojure_noob/fibonacci.clj
+++ src/clojure_noob/fibonacci.clj
@@ -4,6 +4,6 @@
   "Calculate Fibonacci value by potency"
   ([] 0)
   ([potency]
-  (if (<= potency 1)
-    potency
-    (+ (calc (- potency 1)) (calc (- potency 2))))))
+   (if (<= potency 1)
+     potency
+     (+ (calc (- potency 1)) (calc (- potency 2))))))
